23a23987d8685d1dffa086d2720cb9216851fefb
nicolasdca <nicolasdavid193@gmail.com>
2024-04-03 07:31:09 -0300

    Desafio Alura Spring 2

---
+++ src/main/java/br/com/nicolas/AluraMusic/AluraMusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.AluraMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraMusicApplication.class, args);
+	}
+
+}
